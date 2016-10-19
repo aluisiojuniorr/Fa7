1b63610b9a4714ea2084e06be4f8bcf91eaf82e3
aluisiojuniorr <aluisiojuniorr@gmail.com>
2016-10-19 13:19:00 -0300

    Testes implementados

--- Fa7Trabalho/Fa7Trabalho/CalcularNota.cs
+++ Fa7Trabalho/Fa7Trabalho/CalcularNota.cs
@@ -6,34 +6,18 @@ using System.Threading.Tasks;
 
 namespace Fa7Trabalho
 {
-    class CalcularNota
+    public class CalcularNota
     {
         #region Exercicio Prova Trabalho Fa7
 
         static void Main(string[] args)
         {
-            double notaAv1;
-            double notaAv2;
-            double notaFinal;
-
-            Console.Write("Nota AV1: ");
-            notaAv1 = double.Parse(Console.ReadLine());
-            Console.Write("Nota AV2: ");
-            notaAv2 = double.Parse(Console.ReadLine());
-
-            notaFinal = (notaAv1 + notaAv2) / 2;
-
-            if (notaFinal >= 7)
-            {
-                Console.Write("Aluno Aprovado com Nota: " + notaFinal);
-
-            }
-            else
-            {
-                Console.Write("Aluno Reprovado com Nota: " + notaFinal);
-            }
-            Console.ReadKey();
         }
+
+        public int Soma(int a, int b)
+        {
+            return a + b;
+        } 
 
         #endregion
     }
---
+++ Fa7Trabalho/Testes/TestraCalcularNota.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fa7Trabalho;
+
+namespace Testes
+{
+    [TestClass]
+    public class TestraCalcularNota
+    {
+        [TestMethod]
+        public void TestarSomaNota()
+        {
+            CalcularNota n = new CalcularNota();
+            int resultado = n.Soma(1, 1);
+            Assert.AreEqual(2, resultado);
+        }
+    }
+}
