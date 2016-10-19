79963f6fb0e8cc738e4039011625e7c54568f5b6
aluisiojuniorr <aluisiojuniorr@gmail.com>
2016-10-19 13:01:04 -0300

    Projeto Trabalho Fa7

---
+++ Fa7Trabalho/Fa7Trabalho/CalcularNota.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fa7Trabalho
+{
+    class CalcularNota
+    {
+        #region Exercicio Prova Trabalho Fa7
+
+        static void Main(string[] args)
+        {
+            double notaAv1;
+            double notaAv2;
+            double notaFinal;
+
+            Console.Write("Nota AV1: ");
+            notaAv1 = double.Parse(Console.ReadLine());
+            Console.Write("Nota AV2: ");
+            notaAv2 = double.Parse(Console.ReadLine());
+
+            notaFinal = (notaAv1 + notaAv2) / 2;
+
+            if (notaFinal >= 7)
+            {
+                Console.Write("Aluno Aprovado com Nota: " + notaFinal);
+
+            }
+            else
+            {
+                Console.Write("Aluno Reprovado com Nota: " + notaFinal);
+            }
+            Console.ReadKey();
+        }
+
+        #endregion
+    }
+}
